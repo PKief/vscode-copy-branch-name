c32ebd7dbd4fdb91d61cc0430ae2328209d9e67f
Philipp Kief <philipp.kief@gmx.de>
2022-08-01 19:27:22 +0200

    Add types

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import clipboard from "clipboardy";
+import * as vscode from "vscode";
+import { GitExtension } from "./@types/vscode.git";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copy-branch-name" is now active!');
+export const activate = (context: vscode.ExtensionContext) => {
+  const copyButton = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Left,
+    99999
+  );
+  copyButton.text = "Copy current branch";
+  copyButton.command = "copy-branch-name.copy-current";
+  copyButton.show();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('copy-branch-name.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from copy-branch-name!');
-	});
+  let disposable = vscode.commands.registerCommand(
+    "copy-branch-name.copy-current",
+    () => {
+      const gitExtension =
+        vscode.extensions.getExtension<GitExtension>("vscode.git")?.exports;
+      if (gitExtension) {
+        const api = gitExtension.getAPI(1);
 
-	context.subscriptions.push(disposable);
-}
+        const repo = api.repositories[0];
+        const branchName = (repo.state.HEAD && repo.state.HEAD.name) || "";
+        clipboard.writeSync(branchName);
+      }
+    }
+  );
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+  context.subscriptions.push(disposable);
+};
+
+export const deactivate = () => {};
